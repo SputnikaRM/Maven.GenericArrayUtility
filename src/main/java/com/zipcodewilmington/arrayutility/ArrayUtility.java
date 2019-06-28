7c548a3f3fc32c262d23c84a42f40e8c5c18157c
sputnika <sputnika91@gmail.com>
2019-06-28 16:43:07 -0400

    update

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,17 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility <T> {
+    private T array[];
+
+    public ArrayUtility(T[] array) {
+        this.array = array;
+    }
+
+    public T countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+
+        return null;
+    }
+
 }
+
