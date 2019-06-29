da33d6f374e66fe60601980527280c85b9b79a42
sputnika <sputnika91@gmail.com>
2019-06-29 17:23:15 -0400

    update

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,5 +1,9 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by leon on 3/6/18.
  */
@@ -10,10 +14,62 @@ public class ArrayUtility <T> {
         this.array = array;
     }
 
-    public T countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
-
-        return null;
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        Integer count = 0;
+        for(int x=0; x< arrayToMerge.length;x++){
+            if(arrayToMerge[x].equals(valueToEvaluate))
+                count++;}
+//        T [] inputArray= array;
+//        for(int x=0;x<inputArray.length;x++){
+//            if(inputArray[x].equals(valueToEvaluate))
+//                count++;
+//        }
+        count = getNumberOfOccurrences(valueToEvaluate)+count;
+        return count;
     }
 
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        Integer count =0;
+        T [] inputArray= array;
+        for(int x=0;x<inputArray.length;x++){
+            if(inputArray[x].equals(valueToEvaluate))
+                count++;
+        }
+        return count;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        List<T> list= new ArrayList<>();
+        T [] inputArray = array;
+        for(int x=0; x<inputArray.length;x++){
+            if(!(inputArray[x].equals(valueToRemove)))
+                list.add(inputArray[x]);
+        }
+
+        return list.toArray(Arrays.copyOf(inputArray,list.size()));
+
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        List<T> list = new ArrayList<>();
+        T [] inputArray=array;
+        T mostCommon = list.get(0);
+        for(int x= 0; x<inputArray.length;x++)
+            list.add(inputArray[x]);
+        for(int x= 0; x<arrayToMerge.length;x++)
+            list.add(arrayToMerge[x]);
+        Integer number =0;
+        for(int x=0; x<list.size()-1;x++){
+           Integer y= getNumberOfOccurrences(list.get(x));
+            if(y > number){
+                number =y;
+                mostCommon=list.get(x);
+            }
+
+
+        }
+
+        return mostCommon;
+    }
 }
 
